892e91235afc8c8a89ca913fff9f669a04a9622d
The6eyes <theofficialkno1@gmail.com>
2024-11-22 11:36:55 -0500

    Initial Commit

---
+++ WeatherDemo/Views/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  WeatherDemo
+//
+//  Created by Fred Saffold on 11/16/24.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Fred Saffold on 11/16/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
